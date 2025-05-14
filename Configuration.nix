8fd2f97f4ad1e461a4f3fa5c62088d13bcbe4d49
Klavs Kalnins <klavskalninss@gmail.com>
2025-05-14 12:14:58 +0300

    nix

--- Configuration.nix
+++ Configuration.nix
@@ -1,3 +1,7 @@
+# notes
+## nixos-rebuild switch // to apply changes
+## nixos-rebuild to create a VM using this configuration and run the result
+
 
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
@@ -42,6 +46,10 @@
     isNormalUser = true;
     description = "kaste";
     extraGroups = [ "networkmanager" "wheel" ];
+    openssh.authorizedKeys.keys = [
+      # Add your public key if you want to SSH *into* this server
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBAgYRyANen+i2kT+kDBx7M/LjwIK8zaua0Y2gzVPjKU nixos-server"
+    ];
     packages = with pkgs; [];
   };
 
@@ -57,6 +65,8 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   git
+  dotnet-sdk_9
+  postgresql_16
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -68,6 +78,51 @@
   # };
 
   # List services that you want to enable:
+  system.activationScripts.setupSSH = ''
+    mkdir -p /home/kaste/.ssh
+    cp /etc/ssh/kaste /home/kaste/.ssh/kaste
+    chmod 600 /home/kaste/.ssh/kaste
+    chown -R kaste:kaste /home/kaste/.ssh
+
+    echo "Host github.com
+      IdentityFile /home/kaste/.ssh/kaste
+      StrictHostKeyChecking no
+      User git" > /home/kaste/.ssh/config
+    chmod 600 /home/kaste/.ssh/config
+  '';
+
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_15;  # Choose version as needed
+    authentication = ''
+      local   all             all                                     trust
+      host    all             all             127.0.0.1/32            md5
+      host    all             all             ::1/128                 md5
+    '';
+    initialScript = pkgs.writeText "init.sql" ''
+      CREATE USER kaste WITH PASSWORD 'your-secure-password';
+      CREATE DATABASE apidb OWNER kaste;
+    '';
+  };
+
+  system.activationScripts.gitClone = ''
+    mkdir -p /home/kaste/myapp
+    chown kaste:kaste /home/kaste/myapp
+    sudo -u kaste git clone https://your-repo-url.git /home/kaste/myapp || true
+  '';
+
+  systemd.services.dotnet-api = {
+    description = "My .NET API";
+    after = [ "network.target" "postgresql.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      User = "kaste";
+      WorkingDirectory = "/home/kaste/myapp";
+      ExecStart = "${pkgs.dotnet-sdk_8}/bin/dotnet run";
+      Restart = "always";
+      Environment = "ASPNETCORE_ENVIRONMENT=Production";
+    };
+  };
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # nix
+
+QEMU
+
+## run on mac
+
+mkdir nixos-vm && cd nixos-vm
+nano configuration.nix
+
+nix-shell -p nixos-rebuild
+nixos-rebuild build-vm -I nixos-config=./configuration.nix
+./result/bin/run-*-vm
+
+## if needed remove nix
+
+### 1. Remove the /nix volume
+
+sudo diskutil apfs deleteVolume /nix
+
+### 2. Remove the mount config
+
+sudo sed -i '' '/^nix$/d' /etc/synthetic.conf
+
+### 3. Remove the launch daemon if it exists
+
+sudo rm -f /Library/LaunchDaemons/org.nixos.nix-daemon.plist
+
+### 4. Delete Nix-related user files
+
+sudo rm -rf /nix ~/.nix* ~/.config/nixpkgs ~/.local/state/nix
+
+### 5. Reboot your system (important to unmount cleanly)
+
+sudo reboot
+
+
+# build and qemu
+
+brew install qemu
+
+nixos-rebuild build-vm -I nixos-config=./configuration.nix
+
+cd ./result
+./bin/run-*-vm
+
+// qemu-system-x86_64 -drive file=./result/nixos.qcow2,format=qcow2 -m 2048 -smp 2 -net nic -net user
